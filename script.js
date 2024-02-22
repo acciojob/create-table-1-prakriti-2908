3b6839d671158584a444009aa6719dd2d92f1b27
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-22 23:38:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+	  let table = document.getElementById("sampleTable");
+   
+      let row = table.insertRow(0); 
+      let c1 = row.insertCell(0);
+      let c2 = row.insertCell(1);
+	c1.innerHTML = "New Cell1";
+	c2.innerHTML = "New Cell2";
   
 }
